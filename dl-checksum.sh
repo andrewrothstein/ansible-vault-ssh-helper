3858291599be8f69346e735a8e13c42844bc788c
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-10-16 21:16:38 -0500

    add checksums. addl CI builds.

---
+++ dl-checksum.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+APP=vault-ssh-helper
+DIR=~/Downloads
+MIRROR=https://releases.hashicorp.com/${APP}
+
+dl()
+{
+    local ver=$1
+    local lchecksums=$2
+    local os=$3
+    local arch=$4
+    local platform=${os}_${arch}
+    local file=${APP}_${ver}_${platform}.zip
+    local url=$MIRROR/$ver/$file
+
+    printf "        # %s\n" $url
+    printf "        %s: sha256:%s\n" $platform `fgrep $file $lchecksums | awk '{print $1}'`
+}
+
+dl_all () {
+    local ver=$1
+    local checksums=${APP}_${ver}_SHA256SUMS
+    local lchecksums=$DIR/$checksums
+    local rchecksums=$MIRROR/$ver/$checksums
+
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $rchecksums
+    fi
+
+    printf "      # %s\n" $rchecksums
+    printf "      '%s':\n" $ver
+    dl $ver $lchecksums linux 386
+    dl $ver $lchecksums linux amd64
+    dl $ver $lchecksums linux arm
+}
+
+dl_all 0.1.0
+dl_all 0.1.1
+dl_all 0.1.2
+dl_all 0.1.3
+dl_all 0.1.4
+
